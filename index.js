4f573b2971451c03f1287bf2eeb7a2bbcec1b9dd
sravan7 <esd14i017@iiitdm.ac.in>
2021-03-03 09:11:45 +0530

    add first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const preciseToNfractionals = (number,digits)=>{
+    try {
+        if(typeof(number)==='number'){
+            if(Number.isInteger(digits) && digits>=0){
+                if(number>=0){
+                    const output = Number(Math.floor(Number(number+`e${digits}`))+`e-${digits}`);
+                    return output;
+                }
+                else{
+                    const output = Number(Math.ceil(Number(number+`e${digits}`))+`e-${digits}`);
+                    return output;
+                }
+            }
+            else{
+                throw 'digits should be valid positive integer'
+            }
+        }
+        else{
+            throw 'number should be valid number'
+        }
+    }
+    catch (err){
+        console.error(err);
+        process.exit(1)
+    }
+}
+module.exports = preciseToNfractionals;
