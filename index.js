70b0fb49e5e44170b9b749873b8c51bc7bccfa54
sravan7 <esd14i017@iiitdm.ac.in>
2021-03-07 09:46:40 +0530

    add engine flag 6.17.1 and use strict

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const preciseToNfractionals = (number,digits)=>{
+"use strict";
+const preciseToNfractionals = (number, digits) => {
     try {
-        if(typeof(number)==='number'){
-            if(Number.isInteger(digits) && digits>=0){
-                if(number>=0){
-                    const output = Number(Math.floor(Number(number+`e${digits}`))+`e-${digits}`);
+        if (typeof (number) === 'number') {
+            if (Number.isInteger(digits) && digits >= 0) {
+                if (number >= 0) {
+                    const output = Number(Math.floor(Number(number + `e${digits}`)) + `e-${digits}`);
                     return output;
                 }
-                else{
-                    const output = Number(Math.ceil(Number(number+`e${digits}`))+`e-${digits}`);
+                else {
+                    const output = Number(Math.ceil(Number(number + `e${digits}`)) + `e-${digits}`);
                     return output;
                 }
             }
-            else{
-                throw 'digits should be valid positive integer'
+            else {
+                throw 'digits should be valid positive integer';
             }
         }
-        else{
-            throw 'number should be valid number'
+        else {
+            throw 'number should be valid number';
         }
     }
-    catch (err){
+    catch (err) {
         console.error(err);
     }
-}
+};
 module.exports = preciseToNfractionals;
